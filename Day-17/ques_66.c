59132a4050eafff422f591e76d3b8c07b5a6ac9e
Vishvendra Singh <vishvendrasinghh09@gmail.com>
2026-06-18 18:17:49 +0530

    assignments uploaded

--- Day-17/ques_66.c
+++ Day-17/ques_66.c
@@ -1,12 +1,12 @@
-//  Write a program to Union of arrays. 
+//wap for union of two arrays.
+
 #include<stdio.h>
 
 int main()
 {   int n1=5,n2=5;
-    int arr1[n1];
-    int arr2[n2];
+    int arr1[n1], arr2[n2];
     int unionsize=0;
-    int i,union_arr[n1 + n2];
+    int i,j,union_arr[n1 + n2];
 
     printf("enter elements of first array :");
     for(i=0;i<n1;i++)
@@ -22,13 +22,23 @@ int main()
        unionsize++;
     }
     for(i=0;i<n2;i++)
-    {
-        union_arr[unionsize]=arr2[i];
-        unionsize++;
+    {   int duplicate=0;
+        for(j=0;j<unionsize;j++)
+        {
+            if (arr2[i]==union_arr[j])
+            {
+                duplicate=1;
+                break;
+            }
+        }
+        if(!duplicate){
+            union_arr[unionsize]=arr2[i];
+            unionsize++;
+        }
     }
     
     printf("union array: ");
-    for(i=0;i<n1+n2;i++)
+    for(i=0;i<unionsize;i++)
     {
         printf("%d ",union_arr[i]);
     }
